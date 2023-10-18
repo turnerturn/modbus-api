d1ae13897ec244b1d75aacf6b2d7f7828ac72dbc
Matt Turner <matthewgeorgeturner@gmail.com>
2023-10-17 22:08:38 -0500

    stuff

--- src/main/java/com/toolbox/modbus/modbusapi/ModbusCommandHandler.java
+++
@@ -1,6 +0,0 @@
-package com.toolbox.modbus.modbusapi;
-
-public abstract class ModbusCommandHandler {
-    public abstract boolean isMine(final ModbusCommand command);
-    public abstract CommandResponse execute(final ModbusCommand command) ;
-}
---
+++ src/test/java/com/toolbox/modbus/modbusapi/ModbusApiApplicationTest.java
@@ -0,0 +1,5 @@
+package com.toolbox.modbus.modbusapi;
+
+public class ModbusApiApplicationTest {
+    
+}
